f993933d3e302605d0978ad94187d14bc6042493
Kimberly Luke <uniquememories_kim@hotmail.com>
2022-07-18 14:32:21 -0400

    Created components and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MerchComponent } from './merch/merch.component';
+import { MerchListComponent } from './merch/merch-list/merch-list.component';
+import { MerchDetailComponent } from './merch/merch-detail/merch-detail.component';
+import { MerchItemComponent } from './merch/merch-list/merch-item/merch-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MerchComponent,
+    MerchListComponent,
+    MerchDetailComponent,
+    MerchItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/merch/merch-list/merch-item/merch-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-merch-item',
+  templateUrl: './merch-item.component.html',
+  styleUrls: ['./merch-item.component.css']
+})
+export class MerchItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
